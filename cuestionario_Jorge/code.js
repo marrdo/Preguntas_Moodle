812ac3cbcf6fed73717d9ff091a98fc770cbaf19
Merunemes <batataasesina@gmail.com>
2023-11-01 09:25:55 +0100

    CuestionarioHalloweenMorning

--- cuestionario_Jorge/code.js
+++ cuestionario_Jorge/code.js
@@ -1,4 +1,101 @@
 "use strict";
 
-//Aquí ya iría todo los de los botones, cuando la página carga, etc.
+//Guardamos los botones y hacemos que validen
 
+let btnCreate = document
+  .getElementById("crear-pregunta")
+  .addEventListener("click", validateForm);
+
+const specialCharacters = ["~", "#", "=", "}", "{", ":"];
+
+const cuestionario = new Cuestionario();
+
+//Función para validar y luego generar preguntas (está en Alpha)
+function validateForm() {
+  let textarea = document.querySelector("#txtPregunta").value;
+  let correctAnswer = document.querySelector("#correctAnswer").value;
+  let firstAnswer = document.querySelector("#firstAnswer").value;
+  let secondAnswer = document.querySelector("#secondAnswer").value;
+  let thirdAnswer = document.querySelector("#thirdAnswer").value;
+  let divErrores = document.getElementById("divErrores");
+  let id = 0;
+
+  if (
+    textarea == "" ||
+    correctAnswer == "" ||
+    firstAnswer == "" ||
+    secondAnswer == "" ||
+    thirdAnswer == ""
+  ) {
+    divErrores.innerHTML = `<h2>ERRORES:</h2>
+     Por favor, rellene todos los campos.`;
+  } else {
+    //ESCAPAR TEXTO
+
+    function escapeSpecialCharacters(text) {
+      specialCharacters.forEach((char) => {
+        const regex = new RegExp(`\\${char}`, "g"); // Use a regular expression to find the special character
+        text = text.replace(regex, `\\${char}`); // Replace it with a backslash and the character
+      });
+
+      return text;
+    }
+
+    // const arrSpecial = ['~','#','=','}','{',':'];
+
+    if (
+      specialCharacters.some((char) => textarea.includes(char)) ||
+      specialCharacters.some((char) => firstAnswer.includes(char)) ||
+      specialCharacters.some((char) => secondAnswer.includes(char)) ||
+      specialCharacters.some((char) => thirdAnswer.includes(char)) ||
+      specialCharacters.some((char) => correctAnswer.includes(char))
+    ) {
+      //HAY CARACTERES ESPECIALES
+      textarea = escapeSpecialCharacters(textarea);
+      firstAnswer = escapeSpecialCharacters(firstAnswer);
+      secondAnswer = escapeSpecialCharacters(secondAnswer);
+      thirdAnswer = escapeSpecialCharacters(thirdAnswer);
+      correctAnswer = escapeSpecialCharacters(correctAnswer);
+
+      alert(textarea+ "está escapao");
+    }
+      // let newQuestion = new Pregunta(id,textarea,correctAnswer,firstAnswer,secondAnswer,thirdAnswer);
+      // id++;
+      //  let cuestionario = new Cuestionario;
+      //  cuestionario.añadirPregunta(newQuestion);
+      //  console.table(cuestionario);      //xD por soñar que no quede
+
+      // Crear una nueva instancia de Pregunta
+      const nuevaPregunta = new Pregunta(
+        id,
+        textarea,
+        correctAnswer,
+        firstAnswer,
+        secondAnswer,
+        thirdAnswer
+      );
+
+      // Añadir la pregunta al cuestionario
+      cuestionario.añadirPregunta(nuevaPregunta);
+
+      // Incrementar el ID para la próxima pregunta
+      id++;
+
+      document.getElementById('divPreguntas').innerHTML = "HOLA";
+
+      const preguntaDiv = cuestionario.preguntaToHTMLDiv(id);
+        
+   
+      // Obtener la representación HTML de la pregunta
+      if (preguntaDiv) {
+        document.getElementById('divPreguntas').appendChild(preguntaDiv);
+      }
+      console.log(textarea, correctAnswer, firstAnswer, secondAnswer, thirdAnswer);
+      console.log(cuestionario);
+      console.log(id);
+
+    
+  }
+
+  return false;
+}
--- cuestionario_Jorge/objetos.js
+++ cuestionario_Jorge/objetos.js
@@ -1,8 +1,3 @@
-"use strict";
-
-//CLASE CUESTIONARIO
-
-
 class Cuestionario {
     constructor() {
         this.preguntas = [];
@@ -22,18 +17,18 @@ class Cuestionario {
 
     preguntaToHTMLDiv(id) {
 
-        /* NOTA: Dejo comentado esto porque se lo pregunté al GPT, pero habría que estudia los otros métodos
-        pa ver si está bien.
+        /* NOTA: Dejo comentado esto porque  habría que estudia los otros métodos
+        pa ver si está bien.*/
 
-        // const pregunta = this.getPregunta(id);
-        // if (pregunta) {
-        //     const preguntaHTML = pregunta.toHTMLUl();
-        //     const div = document.createElement('div');
-        //     div.appendChild(preguntaHTML);
-        //     return div;
-        // } else {
-        //     return null; // Pregunta no encontrada
-        // }*/
+        const pregunta = this.getPregunta(id);
+        if (pregunta) {
+            const preguntaHTML = pregunta.toHTMLUl();
+            const div = document.createElement('div');
+            div.appendChild(preguntaHTML);
+            return div;
+        } else {
+            return null; // Pregunta no encontrada
+        }
     }
 }
 
@@ -42,43 +37,44 @@ class Cuestionario {
 
 class Pregunta{
 
-    constructor(id, texto,respuestaCorrecta,respuestasIncorrectas) {
+    constructor(id, texto,respuestaCorrecta,respuestasIncorrecta1, respuestasIncorrecta2,respuestasIncorrecta3) {
         this.id = id;
         this.texto = texto;
         this.respuestaCorrecta = respuestaCorrecta;
-        this.respuestasIncorrectas = respuestasIncorrectas
+        this.respuestasIncorrecta1 = respuestasIncorrecta1;
+        this.respuestasIncorrecta2 = respuestasIncorrecta2;
+        this.respuestasIncorrecta3 = respuestasIncorrecta3;
       }
 
       toHTMLUl() {
 
-        //LO MISMO QUE CON EL MÉTODO DE CUESTIONARIO
-
-        // const ul = document.createElement('ul');
+   
+        const ul = document.createElement('ul');
         
-        // // Agregar el texto de la pregunta
-        // const liTexto = document.createElement('li');
-        // liTexto.textContent = `Pregunta: ${this.texto}`;
-        // ul.appendChild(liTexto);
+        // Agregar el texto de la pregunta
+        const liTexto = document.createElement('li');
+        liTexto.textContent = `Pregunta: ${this.texto}`;
+        ul.appendChild(liTexto);
 
-        // // Agregar la respuesta correcta
-        // const liRespuestaCorrecta = document.createElement('li');
-        // liRespuestaCorrecta.textContent = `Respuesta Correcta: ${this.respuestaCorrecta}`;
-        // ul.appendChild(liRespuestaCorrecta);
+        // Agregar la respuesta correcta
+        const liRespuestaCorrecta = document.createElement('li');
+        liRespuestaCorrecta.textContent = `Respuesta Correcta: ${this.respuestaCorrecta}`;
+        ul.appendChild(liRespuestaCorrecta);
 
-        // // Agregar las respuestas incorrectas
-        // const liRespuestasIncorrectas = document.createElement('li');
-        // liRespuestasIncorrectas.textContent = 'Respuestas Incorrectas:';
-        // ul.appendChild(liRespuestasIncorrectas);
+        // Agregar las respuestas incorrectas
+        const liRespuestasIncorrectas = document.createElement('li');
+        liRespuestasIncorrectas.textContent = 'Respuestas Incorrectas:';
+        ul.appendChild(liRespuestasIncorrectas);
 
-        // const ulRespuestasIncorrectas = document.createElement('ul');
-        // this.respuestasIncorrectas.forEach(respuesta => {
-        //     const liRespuestaIncorrecta = document.createElement('li');
-        //     liRespuestaIncorrecta.textContent = respuesta;
-        //     ulRespuestasIncorrectas.appendChild(liRespuestaIncorrecta);
-        // });
-        // liRespuestasIncorrectas.appendChild(ulRespuestasIncorrectas);
+        const ulRespuestasIncorrectas = document.createElement('ul');
+        this.respuestasIncorrectas.forEach(respuesta => {
+            const liRespuestaIncorrecta = document.createElement('li');
+            liRespuestaIncorrecta.textContent = respuesta;
+            ulRespuestasIncorrectas.appendChild(liRespuestaIncorrecta);
+        });
+        liRespuestasIncorrectas.appendChild(ulRespuestasIncorrectas);
 
-        // return ul;
+        return ul;
     }
 
 
